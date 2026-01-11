e0739786a3adcd55dd9ae7820f4d2968e89b70d4
kaname <djessy-alberto.kita@etu.univ-lyon1.fr>
2026-01-11 03:13:01 +0100

    modifs encore

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
       <div>
         <header className="hero-section">
         <h1>It's a pleasure to meet you !</h1>
@@ -26,7 +21,6 @@ function App() {
         </p>
       </section>
       </div>
-    </>
   )
 }
 
